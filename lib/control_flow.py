7d2366097eb10da187291c6c34f131cf9726d35a
StormRobert <stormarasa2@gmail.com>
2023-11-24 01:02:55 +0300

    temperature recorded

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -10,7 +10,15 @@ def admin_login(username, password):
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature == 33:
+     return "It's brisk out there!"
+    elif temperature == 55:
+       return "It's a little chilly out there!"
+    elif temperature == 99:
+       return "It's too dang hot out there!"
+    else :
+       return "It's perfect out there!"
+    
 
 def fizzbuzz(num):
     # your code here
