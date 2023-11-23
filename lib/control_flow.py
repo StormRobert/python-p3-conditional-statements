6f1329aa710f15dbaa69c2d2b2db09c80a1a3806
StormRobert <stormarasa2@gmail.com>
2023-11-24 01:08:05 +0300

    Divisble by 3 and 5

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -22,7 +22,14 @@ def hows_the_weather(temperature):
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if (num%3 == 0 and num%5 == 0):
+       return "FizzBuzz"
+    elif (num %3 == 0):
+       return "Fizz"
+    elif (num %5 == 0):
+       return "Buzz"
+    else:
+       return "num"
 
 def calculator(operation, num1, num2): 
     # your code here
