d0ebbdb8f4b5cc37fc43a1ff93457c16f9413282
StormRobert <stormarasa2@gmail.com>
2023-11-24 00:40:59 +0300

    user and password done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,7 +2,11 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if username.lower() == "admin" and password == "12345":
+     return "Access granted"
+    else :
+       return "Access denied"
+    
 
 def hows_the_weather(temperature):
     # your code here
@@ -12,6 +16,6 @@ def fizzbuzz(num):
     # your code here
     pass
 
-def calculator(operation, num1, num2):
+def calculator(operation, num1, num2): 
     # your code here
     pass
